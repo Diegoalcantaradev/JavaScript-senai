7bce5644db8281389931f4226c6da37a620ffcce
Luis Felipe <142508578+felipemaatias@users.noreply.github.com>
2024-08-05 13:57:41 -0300

    ok

---
+++ Aula09/script.js
@@ -0,0 +1,15 @@
+/*
+MÉTADOS AVANÇADOS DE ARRAY
+*/
+
+// (1) - FOREACH(): executa uma data função em cada elemento do array
+
+let array = [10,5,8,15]
+
+array.forEach((elemento) => console.log(elemento))
+
+// Resultado: exibir cada elemento do array no terminal [10,5,8,15]
+
+// (2) - Map(): criar um novo array com os resultados da função de callback passada por argumento
+
+array.map()
